dd3c889aaca0169ce6b34e0614b97797efb3c5ee
root <root@dockerserver.home.com>
2020-10-12 21:28:55 +0530

    For Testing

--- script.sh
+++ script.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/bash
-
-echo "server_IP,server_status,Ram_Usage,Process_List" > ram_usage.csv
-
-for line in `cat inputs.csv`
+echo "Server_IP, Server_Status, Ram_Usage, Process_List" > ram_usage.csv # creating csv file with heads
+for line in `cat inputs.csv`                              # reading the inputs.csv file for inputs
 do
     n=`echo $line | awk -F"," '{print $1}'`
     echo $n
-    m=`echo $line |awk -F"," '{print $2}'`
-    echo $m
-    if [ -z $m ]
+    m=`echo $line | awk -F"," '{print $2}'`
+    if [ -z "$m" ]
     then
         m=3
     fi
-    ping -c 1 $n >/dev/null
-    PingResult=`echo $?`
-    if [ $PingResult -eq 0 ]
+    echo $m
+    ping -c 1 $n > /dev/null
+    pingResult=`echo $?`
+    if [ $pingResult -eq 0 ]
     then
-        serverstatus="Rechable"
+        serverStatus="Reachable"
     else
-        serverstatus="Unrechable"
+        serverStatus="Unreachable"
     fi
-    Memory=`ssh root@$n "free -m "|grep "Mem:"`
-    Memory_Usage=`echo $Memory |awk '{print $3 }'`
-    let m=m+1
-    ProcList=`ps aux --sort -rss|head -n $m |grep -v "USER"|awk '{print $11}'`
-    echo $ProcList
-    echo "$n,$serverstatus,$Memory_Usage,$ProcList">>ram_usage.csv
-done
+    echo $serverStatus
+    Memory=` ssh root@$n "free -m | grep "Mem:""`
+    Memory_Usage=`echo $Memory | awk '{print $3}'`
+    echo $Memory_Usage
+    m=$(($m+1))
+    procList=`ps aux --sort -rss | head -n $m | grep -v "USER" | awk '{print $11}'`
+    echo $procList
+    echo  "$n, $serverStatus, $Memory_Usage, "$procList"" >> ram_usage.csv 
+    
+done 
